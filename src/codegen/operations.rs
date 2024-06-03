30fcf13d581159758e47e3d1e1c530f86514c596
ricomateo <mrico@fi.uba.ar>
2024-06-03 17:58:42 -0300

    correct gas cost

--- src/codegen/operations.rs
+++ src/codegen/operations.rs
@@ -968,7 +968,7 @@ fn codegen_pop<'c, 'r>(
     // Check there's at least 1 element in stack
     let flag = check_stack_has_at_least(context, &start_block, 1)?;
 
-    let gas_flag = consume_gas(context, &start_block, 3)?;
+    let gas_flag = consume_gas(context, &start_block, 2)?;
 
     let condition = start_block
         .append_operation(arith::andi(gas_flag, flag, location))
