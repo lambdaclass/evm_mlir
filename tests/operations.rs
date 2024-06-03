225b674d900fbace9c795d6c5e654c2acc4b9626
tomasarrachea <tomas.arrachea@lambdaclass.com>
2024-06-03 18:43:14 -0300

    feat: add gas cost to MOD opcode

--- tests/operations.rs
+++ tests/operations.rs
@@ -793,6 +793,18 @@ fn mod_with_stack_underflow() {
 }
 
 #[test]
+fn mod_reverts_when_program_runs_out_of_gas() {
+    let (a, b) = (BigUint::from(5_u8), BigUint::from(10_u8));
+    let mut program: Vec<Operation> = vec![];
+    for _ in 0..1000 {
+        program.push(Operation::Push(a.clone()));
+        program.push(Operation::Push(b.clone()));
+        program.push(Operation::Mod);
+    }
+    run_program_assert_revert(program);
+}
+
+#[test]
 fn addmod_with_non_zero_result() {
     let (a, b, den) = (
         BigUint::from(13_u8),
