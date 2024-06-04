ab97a3678102afbe7b3456b9d32e3d1f3dc7d7a1
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-06-04 09:54:53 -0300

    fix: improve "target not supported" check

--- src/lib.rs
+++ src/lib.rs
@@ -256,7 +256,7 @@ pub fn link_binary(
 
             args
         }
-        #[cfg(target_os = "windows")]
+        #[cfg(not(any(target_os = "macos", target_os = "linux")))]
         {
             unimplemented!()
         }
@@ -326,7 +326,7 @@ pub fn link_shared_lib(
 
             args
         }
-        #[cfg(target_os = "windows")]
+        #[cfg(not(any(target_os = "macos", target_os = "linux")))]
         {
             unimplemented!()
         }
