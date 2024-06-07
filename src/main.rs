38dacef299bca058ef961c54f937e80cb7f715ad
Julian Ventura <julian.ventura@lambdaclass.com>
2024-06-07 12:23:45 -0300

    Add error handling to bytecode parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 use std::path::PathBuf;
 
-use evm_mlir::{context::Context, executor::Executor, program::Program, syscall::SyscallContext};
+use evm_mlir::{
+    context::Context,
+    executor::Executor,
+    program::{OpcodeParseError, Program},
+    syscall::SyscallContext,
+};
+
+fn log_failed_opcodes(failed_opcodes: Vec<OpcodeParseError>) {
+    eprintln!("Failed opcodes: {:#?}", failed_opcodes);
+}
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -8,12 +17,17 @@ fn main() {
     let bytecode = std::fs::read(path).expect("Could not read file");
     let program = Program::from_bytecode(&bytecode);
 
+    if let Err(failed_opcodes) = program {
+        log_failed_opcodes(failed_opcodes);
+        return;
+    }
+
     // This is for intermediate files
     let output_file = PathBuf::from("output");
 
     let context = Context::new();
     let module = context
-        .compile(&program, &output_file)
+        .compile(&program.unwrap(), &output_file)
         .expect("failed to compile program");
 
     let executor = Executor::new(&module);
