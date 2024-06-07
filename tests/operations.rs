bb7b77e8d4216748d72034b480346efbdf4d7590
ricomateo <mrico@fi.uba.ar>
2024-06-07 10:19:14 -0300

    improve jumpi test

--- tests/operations.rs
+++ tests/operations.rs
@@ -1615,18 +1615,20 @@ fn jumpi_with_gas_cost() {
     // this test is equivalent to the following program
     // [00] PUSH1 0
     // [02] PUSH1 1
-    // [04] PUSH1 7
+    // [04] PUSH1 9
     // [06] JUMPI
-    // [07] JUMPDEST
-    let pc = BigUint::from(7_u8);
+    // [07] PUSH1 10   // this should not be executed
+    // [09] JUMPDEST
+    let pc = 9;
     let condition = BigUint::from(1_u8);
     let expected_result: u8 = 0;
     let program = vec![
         Operation::Push(BigUint::from(expected_result)),
         Operation::Push(condition),
-        Operation::Push(pc),
+        Operation::Push(BigUint::from(pc)),
         Operation::Jumpi,
-        Operation::Jumpdest { pc: 7 },
+        Operation::Push(BigUint::from(10_u8)), // this should not be executed
+        Operation::Jumpdest { pc },
     ];
     let needed_gas = gas_cost::PUSHN * 3 + gas_cost::JUMPI + gas_cost::JUMPDEST;
     run_program_assert_gas_exact(program, expected_result, needed_gas as _);
