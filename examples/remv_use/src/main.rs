b9ee53f746f92f8036bf42d3fdf296b3f09f6e54
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-06-07 15:15:23 -0300

    a

--- examples/remv_use/src/main.rs
+++ examples/remv_use/src/main.rs
@@ -7,6 +7,8 @@ use revm::{
 const PROGRAM: Bytes = bytes!("7f00000000000000000000000000000000000000000000000000000000000003e75f60015b82156039578181019150909160019003916024565b9150505f5260205ff3");
 
 fn main() {
+    const RUNS: usize = 10000;
+
     let raw = Bytecode::new_raw(PROGRAM.into());
     let mut evm = Evm::builder()
         .with_db(BenchmarkDB::new_bytecode(raw))
@@ -17,7 +19,7 @@ fn main() {
         })
         .build();
 
-    for _ in 0..1000 {
+    for _ in 0..RUNS {
         let result = evm.transact().unwrap();
         assert!(result.result.is_success());
     }
