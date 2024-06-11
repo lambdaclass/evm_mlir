697cbc3eedbf62a0c81c3f29f3776ce19dcb1ffa
FrancoGiachetta <francogiachetta27@gmail.com>
2024-06-11 17:18:55 -0300

    making changes on Db, adding AccountInfo

--- src/db.rs
+++ src/db.rs
@@ -1,13 +1,22 @@
 use std::collections::HashMap;
 
-use ethereum_types::U256;
+use ethereum_types::{H256, U256};
 
+use crate::env::Address;
+
+#[derive(Clone, Debug, Default)]
 pub struct Bytecode(Vec<u8>);
 
 #[derive(Clone, Debug, Default)]
+struct AccountInfo {
+    nonce: u64,
+    balance: U256,
+    storage: HashMap<U256, U256>,
+    bytecode: Bytecode,
+}
+
+#[derive(Clone, Debug, Default)]
 pub struct Db {
-    pub nonce: u64,
-    pub balance: U256,
-    pub storage: HashMap<U256, U256>,
-    pub bytecode: Bytecode,
+    accounts: HashMap<Address, AccountInfo>,
+    bytecode: HashMap<H256, Bytecode>,
 }
