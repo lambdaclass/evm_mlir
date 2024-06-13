9f33ab61c80b32835f348a3a63098be3a3268129
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-06-13 15:41:58 -0300

    fix: use calldata opcodes in fibonacci benchmark (#257)

--- bench/revm_comparison/src/lib.rs
+++ bench/revm_comparison/src/lib.rs
@@ -8,6 +8,11 @@ use revm::{
 };
 use std::{hint::black_box, path::PathBuf};
 
+pub const FIBONACCI_BYTECODE: &str =
+    "5f355f60015b8215601a578181019150909160019003916005565b9150505f5260205ff3";
+pub const FACTORIAL_BYTECODE: &str =
+    "5f355f60015b8215601b57906001018091029160019003916005565b9150505f5260205ff3";
+
 pub fn run_with_evm_mlir(program: &str, runs: usize, number_of_iterations: u32) {
     let bytes = hex::decode(program).unwrap();
     let program = Program::from_bytecode(&bytes).unwrap();
