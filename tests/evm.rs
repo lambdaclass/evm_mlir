ed06026d8883ac78480bc3e148947bf2cf935654
ricomateo <mrico@fi.uba.ar>
2024-06-18 11:24:01 -0300

    feat: add tests for basefee

--- tests/evm.rs
+++ tests/evm.rs
@@ -454,3 +454,30 @@ fn callvalue_stack_overflow() {
     let env = Env::default();
     run_program_assert_halt(program, env);
 }
+
+#[test]
+fn basefee() {
+    let program = vec![Operation::Basefee];
+    let mut env = Env::default();
+    let basefee = 1_u8;
+    let expected_result = BigUint::from(basefee);
+    env.block.basefee = ethereum_types::U256::from(basefee);
+
+    run_program_assert_result(program, env, expected_result);
+}
+
+#[test]
+fn basefee_gas_check() {
+    let program = vec![Operation::Basefee];
+    let needed_gas = gas_cost::BASEFEE;
+    let env = Env::default();
+    run_program_assert_gas_exact(program, env, needed_gas as _);
+}
+
+#[test]
+fn basefee_stack_overflow() {
+    let mut program = vec![Operation::Push0; 1024];
+    program.push(Operation::Basefee);
+    let env = Env::default();
+    run_program_assert_halt(program, env);
+}
