338d027f0441a391325d7d86e8aa77ecaad65445
alessandromazza <alessandro98.mazza@gmail.com>
2024-06-21 00:09:42 +0200

    remove duplictes

--- src/constants.rs
+++ src/constants.rs
@@ -62,13 +62,7 @@ pub mod gas_cost {
     pub const PUSHN: i64 = 3;
     pub const DUPN: i64 = 3;
     pub const SWAPN: i64 = 3;
-    pub const BYTE: i64 = 3;
-    pub const CALLDATACOPY: i64 = 3;
-    pub const CALLDATALOAD: i64 = 3;
-    pub const CALLDATASIZE: i64 = 2;
     pub const TIMESTAMP: i64 = 2;
-    pub const CALLVALUE: i64 = 2;
-    pub const JUMPI: i64 = 10;
     pub const LOG: i64 = 375;
 
     pub fn memory_expansion_cost(last_size: u32, new_size: u32) -> i64 {
