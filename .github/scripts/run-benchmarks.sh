874e17f16f31a191d949dc190d9f0e730e1b269a
Julian Ventura <43799596+JulianVentura@users.noreply.github.com>
2024-07-02 13:37:37 -0300

    Fix typo
    Co-authored-by: Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>

--- .github/scripts/run-benchmarks.sh
+++ .github/scripts/run-benchmarks.sh
@@ -2,7 +2,7 @@
 
 
 echo "# Benchmarking results" > bench-hyperfine.md
-sudo swapoff -a # Disabling swap memory to reduce noice
+sudo swapoff -a # Disabling swap memory to reduce noise
 for program in factorial fibonacci;
 do
     hyperfine -w 5 -r 10 -N --export-markdown "bench-${program}.md" \
