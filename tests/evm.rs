9cf5a5830b794dae85cc14fd4449d74348eb6cf6
Julian Ventura <julian.ventura@lambdaclass.com>
2024-07-12 11:14:07 -0300

    Add `STATICCALL` test using `CREATE2`

--- tests/evm.rs
+++ tests/evm.rs
@@ -3236,7 +3236,7 @@ fn staticcall_state_modifying_revert_with_callee_ops(callee_ops: Vec<Operation>)
     run_program_assert_num_result(env, db, expected_result);
 }
 
-//TODO: Add CREATE2, SELFDESTRUCT
+//TODO: Add SELFDESTRUCT
 #[test]
 fn staticcall_with_sstore_reverts() {
     let operations = vec![
@@ -3293,6 +3293,30 @@ fn staticcall_with_create_reverts() {
         Operation::Push((1, BigUint::from(offset))),
         Operation::Push((1, BigUint::from(size))),
         Operation::Create,
+    ];
+    append_return_result_operations(&mut operations);
+    staticcall_state_modifying_revert_with_callee_ops(operations)
+}
+
+#[test]
+fn staticcall_with_create2_reverts() {
+    let value: u8 = 10;
+    let offset: u8 = 19;
+    let size: u8 = 13;
+    let salt: u8 = 52;
+    // Code that returns the value 0xffffffff
+    let initialization_code = hex::decode("63FFFFFFFF6000526004601CF3").unwrap();
+    let mut operations = vec![
+        // Store initialization code in memory
+        Operation::Push((13, BigUint::from_bytes_be(&initialization_code))),
+        Operation::Push((1, BigUint::ZERO)),
+        Operation::Mstore,
+        // Create
+        Operation::Push((1, BigUint::from(salt))),
+        Operation::Push((1, BigUint::from(size))),
+        Operation::Push((1, BigUint::from(offset))),
+        Operation::Push((1, BigUint::from(value))),
+        Operation::Create2,
     ];
     append_return_result_operations(&mut operations);
     staticcall_state_modifying_revert_with_callee_ops(operations)
