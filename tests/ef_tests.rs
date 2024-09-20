02fb08d7f6e2cb8e6a4199c02a761d4817c6f2db
Juani Medone <juan.medone@lambdaclass.com>
2024-09-20 17:22:02 -0300

    Modify comment

--- tests/ef_tests.rs
+++ tests/ef_tests.rs
@@ -84,7 +84,7 @@ fn get_ignored_suites() -> HashSet<String> {
     HashSet::from([
         "ValueOverflow".into(),      // TODO: parse bigint tx value
         "ValueOverflowParis".into(), // TODO: parse bigint tx value
-        "blake2B".into(), // this is tested within the blake2B binary because takes like 37 min
+        "blake2B".into(), // this is tested in the blake2B binary because takes a lot of time
     ])
 }
 
